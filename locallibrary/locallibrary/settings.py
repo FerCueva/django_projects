fc3357c0610bffff3c5209fc813ed389ade35edd
FerCueva <fercueva@umich.edu>
2019-01-15 22:48:12 +0000

    Skeleton tutorial complete

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f3)-qnql@+^)l)(q)-p6$sb^8o_=s9vce$vsabvvo6#84ow7+k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
