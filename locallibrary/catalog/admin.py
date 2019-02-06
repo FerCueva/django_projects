bf7877dbe29a7675d2ed8b5b0eded95297efc372
FerCueva <fercueva@umich.edu>
2019-02-06 00:39:19 +0000

    User Tutorial Complete

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -8,7 +8,7 @@ admin.site.register(Language)
 # admin.site.register(Author)
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name', 'first_name','date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # Register the admin class with the associated model
@@ -27,12 +27,13 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance) 
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
